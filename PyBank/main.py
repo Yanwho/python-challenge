6c46f87f288df2748a9bdaebf2893ecc9192c6a2
Ed Yancik <68304939+Yanwho@users.noreply.github.com>
2020-09-08 15:26:56 -0600

    removed comments used while fumbling through code writing

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,35 +3,20 @@ import csv
 from typing import List
 
 filepath = os.path.join("budget_data.csv")
-OUT_PATH = os.path.join("Analysis", "financial_analysis.txt")
+report_file = os.path.join("Analysis", "financial_analysis.txt")
 
-prior_month = 0
-
-best_month = 0
+# variables that work
 best_gain = 0
-worst_month = 0
 worst_loss = 0
-
-sum_month = 0
 profit_difference = []
-initial_month = 0
 sum_profits = 0
 month_count = 0
-monthly_change = []
-net_change_list = []
+
+# variables I can't get working:
 greatest_increase = float(-99999999999999)
 greatest_decrease = float(999999999999)
-
-# The total number of months included in the dataset
-#
-# The net total amount of "Profit/Losses" over the entire period
-#
-# The average of the changes in "Profit/Losses" over the entire period
-#
-# The greatest increase in profits (date and amount) over the entire period
-#
-# The greatest decrease in losses (date and amount) over the entire period
-
+best_month = 0
+worst_month = 0
 
 # create output file
 output_file = "budget_analysis.txt"
@@ -40,20 +25,10 @@ with open("budget_data.csv", "r") as file:
     print("file: ", file)
     reader = csv.DictReader(file)
 
-
-
-
-    # print("reader: ", reader)
     print(10 * "=")
-
 
 #  iterate through the file and perform calculations    
     for row in reader:
-
-        # track the month count
-
-        # track the total
-        # sum_profits += int(row[1])
         month_count += 1
         sum_profits += float(row["Profit/Losses"])
         if month_count == 1:
@@ -65,26 +40,26 @@ with open("budget_data.csv", "r") as file:
             worst_loss = min(profit_difference)
             best_gain = max(profit_difference)
 
-    average_change = (sum(profit_difference) / len(profit_difference))
-    formatted_difference = "${:,.2f}".format(average_change)
+average_change = (sum(profit_difference) / len(profit_difference))
+formatted_difference = "${:,.2f}".format(average_change)
 
-print(40 * "*")
-
-# print the profit and gains/losses in a currency format with commas 2 decimal places
 formatted_profit = "${:,.2f}".format(sum_profits)
 formatted_worst_loss = "${:,.2f}".format(worst_loss)
 formatted_best_gain = "${:,.2f}".format(best_gain)
-print("Sum Profits: ", formatted_profit)
-print("Month Count: ", month_count)
-print("Average Profit Difference: ", formatted_difference)
-# cannot get the best and worst month to print
-print("Best Month: ", best_month)
-print("Best Gain: ", formatted_best_gain)
-print("Worst Month: ",worst_month)
-print("Worst Loss: ", formatted_worst_loss)
-print(40 * "*")
+# # # printing of results for easy view in PyCharm without bouncing to and from file
+# # print the profit and gains/losses in a currency format with commas 2 decimal places
+# print(40 * "*")
+# print("Sum Profits: ", formatted_profit)
+# print("Month Count: ", month_count)
+# print("Average Profit Difference: ", formatted_difference)
+# # cannot get the best and worst month to print
+# print("Best Month: ", best_month)
+# print("Best Gain: ", formatted_best_gain)
+# print("Worst Month: ",worst_month)
+# print("Worst Loss: ", formatted_worst_loss)
+# print(40 * "*")
 # writing the findings to the report file
-with open(OUT_PATH, "w") as txt:
+with open(report_file, "w") as txt:
     txt.write("Financial Analysis:" + "\n")
     txt.write(40 * "-" + "\n")
     txt.write("Sum Profits: " + formatted_profit  + "\n")
