3c26c867f22139908531bc59bb0c7d736e7e3f76
Ed Yancik <68304939+Yanwho@users.noreply.github.com>
2020-09-10 16:36:29 -0600

    cleaned up broken code that was tried and commented out.  added descriptive comments to code blocks.  Finished!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,15 @@
 import os
 import csv
 
+# naming varibles
 candidate = []
 vote_total = 0
 candidates_count = {}
-
-# print("candidates_count: ")
-# print(type(candidates_count))
-
+candidate_list = {}
 filepath = os.path.join("resources", "election_data.csv")
 report_file = os.path.join("Analysis", "results.txt")
 
-# Split the data on commas since that is your delimiter
+# creating list of candidates from dataset
 with open(filepath) as file:
     reader = csv.reader(file)
     next(reader)
@@ -19,88 +17,51 @@ with open(filepath) as file:
         vote_total += 1
         candidate.append(row[2])
 
-
-
+# counting votes per candidate
 candidates_count = [[count_of, candidate.count(count_of)] for count_of in set(candidate)]
-
 candidates_count.sort()
-candidate_list = {}
-# max_key = max(candidates_count, key=candidates_count.get)
-# print(max_key)
+
+# creating dictionary to identify winning vote getter by max element
 for new in candidates_count:
     candidate_list[new[0]] = new[1:]
 print(candidate_list)
 print(type(candidate_list))
 winner = max(candidate_list, key=candidate_list.get)
 
-
-# zip(candidate_list)
-# print(candidate_list)
-# if int(candidates_count[0][1]) > int(candidates_count[1][1]):
-#     winner = candidates_count[0]
-# elif int(candidates_count[0][1]) > int(candidates_count[2][1]):
-#     winner = candidates_count[0]
-# elif int(candidates_count[0][1]) > int(candidates_count[3][1]):
-#     winner = candidates_count[0]
-# print(winner)
-
-        # vote_total += 1
-        # voter_id = row["Voter ID"]
-        # county = row["County"]
-        # candidate_name = row["Candidate"]
-
-        # if candidate_name not in candidates:
-        #     candidates.append(candidate_name)
-        #     print(candidates)
-
-
-        # candidates_count = candidates.count(candidate_name)
-        #
-        # print(candidates_count)
-
-# print(candidate)
-# print(candidates_count)
-# print(candidates_count)
-
+# prints to check code on the fly
 print("Election Results")
 print("-" * 25)
 print("Total Votes: ", vote_total)
 print("-" * 25)
+print(winner + " is the winner!")
 
-# print(candidates_count[0])
+# isolating candidate vote count and calculating % of total vote
+# would NOT work if i didnt know all of the candidates
 candidate_0 = (candidates_count[0])
 c = (candidate_0[0])
 d = (candidate_0[1])
 percent_won = round(((d) / vote_total) *100)
-print(f"{c}: {percent_won}% {d}")
 candidate_0_result = (f"{c}: {percent_won}% {d}")
 
 candidate_1 = (candidates_count[1])
 c = (candidate_1[0])
 d = (candidate_1[1])
 percent_won = round(((d) / vote_total) *100)
-print(f"{c}: {percent_won}% {d}")
 candidate_1_result = (f"{c}: {percent_won}% {d}")
-
 
 candidate_2 = (candidates_count[2])
 c = (candidate_2[0])
 d = (candidate_2[1])
 percent_won = round(((d) / vote_total) *100)
-print(f"{c}: {percent_won}% {d}")
 candidate_2_result = (f"{c}: {percent_won}% {d}")
-
 
 candidate_3 = (candidates_count[3])
 c = (candidate_3[0])
 d = (candidate_3[1])
 percent_won = round(((d) / vote_total) *100)
-print(f"{c}: {percent_won}% {d}")
 candidate_3_result = (f"{c}: {percent_won}% {d}")
 
-print(winner + " is the winner!")
-
-
+# write resulting text file
 with open(report_file, "w") as txt:
     txt.write("Election Results" + "\n")
     txt.write(25 * "-" + "\n")
@@ -113,5 +74,3 @@ with open(report_file, "w") as txt:
     txt.write(25 * "-" + "\n")
     txt.write("Winner: " + str(winner) + "\n")
     txt.write(25 * "-")
-
-
